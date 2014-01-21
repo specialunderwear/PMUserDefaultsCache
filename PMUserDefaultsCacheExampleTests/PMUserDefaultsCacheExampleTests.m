2fef0f53c09a064591143b41f83414cca3e3897a
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-01-21 14:23:12 +0100

    Added tests and podfile and stuff.

--- PMUserDefaultsCacheExampleTests/PMUserDefaultsCacheExampleTests.m
+++ PMUserDefaultsCacheExampleTests/PMUserDefaultsCacheExampleTests.m
@@ -7,28 +7,61 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "PMUserDefaultsCache.h"
 
-@interface PMUserDefaultsCacheExampleTests : XCTestCase
+@interface PMUserDefaultsCacheTests : XCTestCase
 
 @end
 
-@implementation PMUserDefaultsCacheExampleTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+@implementation PMUserDefaultsCacheTests
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // Tear-down code here.
+    [[PMUserDefaultsCache sharedInstance]->_groupedObjects removeAllObjects];
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"koe"];
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"henk"];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"koe"];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"henk"];
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testCachedValue {
+    [PMUserDefaultsCache cacheValue:@1 forKey:@"koe"];
+    [PMUserDefaultsCache cacheValue:@11 forKey:@"henk"];
+    XCTAssertEqualObjects([PMUserDefaultsCache valueForKey:@"koe"], @1, @"value can be stored correctly");
+    XCTAssertEqualObjects([PMUserDefaultsCache valueForKey:@"henk"], @11, @"value can be stored correctly");
+}
+
+- (void)testStoredValue {
+    [PMUserDefaultsCache storeValue:@1 forKey:@"koe"];
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"koe"];
+    XCTAssertEqualObjects([PMUserDefaultsCache valueForKey:@"koe"], @1, @"value is stored in userdefaults so invalidating cache only has no effect on result");
+    [[NSUserDefaults standardUserDefaults] setObject:@11 forKey:@"koe"];
+    XCTAssertEqualObjects([PMUserDefaultsCache valueForKey:@"koe"], @1, @"Changing value stored in NSUSerdefaults changes nothing because value is cached automatically.");
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"koe"];
+    XCTAssertEqualObjects([PMUserDefaultsCache valueForKey:@"koe"], @11, @"After invalidating cached value, the changed value in userdefaults is shown");}
+
+- (void)testInvalidate {
+    [self testCachedValue];
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"koe"];
+    XCTAssertNil([PMUserDefaultsCache valueForKey:@"koe"], @"invalidating a cached item should make the cache respond with nil");
+}
+
+- (void)testInvalidateRelated {
+    [self testCachedValue];
+    [[PMUserDefaultsCache sharedInstance] makeSureToInvalidateKeys:@[@"henk"] whenInvalidatingKey:@"koe"];
+    [[PMUserDefaultsCache sharedInstance] invalidateKey:@"koe"];
+    XCTAssertNil([PMUserDefaultsCache valueForKey:@"koe"], @"invalidating a cached item should make the cache respond with nil");
+    XCTAssertNil([PMUserDefaultsCache valueForKey:@"henk"], @"invalidating a cached item should make the related items get invalidated as well");
+}
+
+- (void)testInvalidateByNilling {
+    [self testCachedValue];
+    [PMUserDefaultsCache storeValue:nil forKey:@"koe"];
+    XCTAssertNil([[NSUserDefaults standardUserDefaults] objectForKey:@"ikbestaniet"], @"Not existing is being nil");
+    XCTAssertNil([[NSUserDefaults standardUserDefaults] objectForKey:@"koe"], @"nilling is deleting");
+    XCTAssertNil([PMUserDefaultsCache valueForKey:@"koe"], @"also in mem it is nil now");
 }
 
 @end
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "PMUserDefaultsCacheExample" do
+    podspec
+end
+
+target "PMUserDefaultsCacheExampleTests" do
+    podspec
+end
+
+inhibit_all_warnings!
